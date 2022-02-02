dd10614a243f891617eb606ceaa86aeab2f0b410
raulsm10 <raulsm10@users.noreply.github.com>
2022-02-02 20:36:56 +0100

    Got entries tags

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-print("Hello World!")
-print("Git")
+from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+
+url_response = urlopen("https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3.atom") # return a http.response object 
+
+tree = ET.parse(url_response)
+root = tree.getroot()
+
+entry_tag = '{http://www.w3.org/2005/Atom}entry'
+
+links = []
+
+for element in root.iter(entry_tag):
+    if element.tag == entry_tag:
+        links.append(element.find('{http://www.w3.org/2005/Atom}link').attrib['href'])
+
+print(len(links))
