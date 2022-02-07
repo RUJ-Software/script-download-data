710a1b9b5959aed00d56ad1d5ab305c5eb241e69
raulsm10 <smraul10@gmail.com>
2022-02-08 00:33:33 +0100

    Inital approach

--- test.py
+++ test.py
@@ -1,17 +1,35 @@
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 
-url_response = urlopen("https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3.atom") # return a http.response object 
-
-tree = ET.parse(url_response)
-root = tree.getroot()
-
-entry_tag = '{http://www.w3.org/2005/Atom}entry'
-
 links = []
+counter = 0
 
-for element in root.iter(entry_tag):
-    if element.tag == entry_tag:
-        links.append(element.find('{http://www.w3.org/2005/Atom}link').attrib['href'])
 
+def searchEntries(url):
+    url_response = urlopen(url) # return a http.response object
+    new_link = ""
+    global counter
+
+    tree = ET.parse(url_response)
+    root = tree.getroot()
+
+    entry_tag = '{http://www.w3.org/2005/Atom}entry'
+
+    for element in root:
+        if element.tag == entry_tag:
+            #links.append(element.find('{http://www.w3.org/2005/Atom}link').attrib['href'])
+            pass
+        if 'rel' in element.attrib and element.attrib['rel'] == 'next':
+            new_link=element.attrib['href']
+
+    if new_link:
+        counter = counter + 1
+        print(counter)
+        searchEntries(new_link)
+    else:
+        print('Search Finished')
+
+
+url = "https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3.atom"
+searchEntries(url)
 print(len(links))
